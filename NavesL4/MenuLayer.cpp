221f5c3793804a50fa3f54dbc698612302c57d3c
fergodani <ferreiragomezdaniel@gmail.com>
2022-11-30 09:53:52 +0100

    Menu de inicio hecho

--- NavesL4/MenuLayer.cpp
+++ NavesL4/MenuLayer.cpp
@@ -8,8 +8,8 @@ MenuLayer::MenuLayer(Game* game)
 
 void MenuLayer::init() {
 	// Fondo normal, sin velocidad
-	background = new Background("res/menu_fondo.png", WIDTH * 0.5, HEIGHT * 0.5, game);
-	button = new Actor("res/boton_jugar.png", WIDTH * 0.5, HEIGHT * 0.7, 232, 72, game);
+	background = new Background("res/menu_fondo.jpg", WIDTH * 0.5, HEIGHT * 0.5, game);
+	button = new Actor("res/boton_jugar.png", WIDTH * 0.5, HEIGHT * 0.7, 180, 90, game);
 }
 
 void MenuLayer::draw() {
@@ -90,8 +90,15 @@ void MenuLayer::mouseToControls(SDL_Event event) {
 	// Cada vez que hacen click
 	if (event.type == SDL_MOUSEBUTTONDOWN) {
 		if (button->containsPoint(motionX, motionY)) {
+			button->texture = game->getTexture("res/boton_jugar_pressed.png");
+			button->draw();
+		}
+	}
+	if (event.type == SDL_MOUSEBUTTONUP) {
+		if (button->containsPoint(motionX, motionY)) {
 			controlContinue = true;
 		}
+
 	}
 }
 
